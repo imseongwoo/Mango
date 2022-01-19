a06fa6b45e944e2442fd8d667a1a2700198faa2e
임성우 <seongwoorang@naver.com>
2022-01-19 16:28:24 +0900

    리사이클러뷰

---
+++ app/src/main/java/org/techtown/mango_contents/ContentsModel.kt
@@ -0,0 +1,11 @@
+package org.techtown.mango_contents
+
+data class ContentsModel (
+
+    val url : String = "",
+    val ImageUrl : String = "",
+    val titleText : String = ""
+
+
+
+        )
--- app/src/main/java/org/techtown/mango_contents/MainActivity.kt
+++ app/src/main/java/org/techtown/mango_contents/MainActivity.kt
@@ -2,10 +2,50 @@ package org.techtown.mango_contents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = mutableListOf<ContentsModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        items.add(
+            ContentsModel(
+                "http://www.mangoplate.com/restaurants/JYZYNprzM8",
+                "https://mp-seoul-image-production-s3.mangoplate.com/37208_1635255570619739.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "고추만두국집"
+
+            )
+        )
+        items.add(
+            ContentsModel(
+                "http://www.mangoplate.com/restaurants/JYZYNprzM8",
+                "https://mp-seoul-image-production-s3.mangoplate.com/37208_1635255570619739.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "고추만두국집"
+
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "http://www.mangoplate.com/restaurants/JYZYNprzM8",
+                "https://mp-seoul-image-production-s3.mangoplate.com/37208_1635255570619739.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "고추만두국집"
+
+            )
+        )
+
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rv)
+        val rvAdapter = RVAdapter(baseContext,items)
+        recyclerView.adapter = rvAdapter
+
+        recyclerView.layoutManager = GridLayoutManager(this,2)
+
     }
 }
